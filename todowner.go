9a7255fce01a20b34d8f85912fa635fbb78595e5
Marios Anagnostopoulos <mariosranag@gmail.com>
2025-01-31 23:24:55 +0200

    Fixes heading indentation and starts processing non heading lines.

--- todowner.go
+++ todowner.go
@@ -44,6 +44,7 @@ func NewLine(content string) *Line {
 // @TODO: If passed file directly do not walk directory
 // @TODO: Add progressbar
 // @TODO: Recursive should be optional (?) -r
+// @TODO: Create doc file to clean up the main function.
 func main() {
 	// Find the .todo files in a given folder, recursively.
 	var filePathsToProcess []string
@@ -71,7 +72,6 @@ func main() {
 	}
 
 	for _, filePath := range filePathsToProcess {
-		// Initialize the file editor.
 		sourceFile, _ := os.Open(filePath)
 		defer sourceFile.Close()
 
@@ -96,7 +96,7 @@ func main() {
 			bufio.NewWriter(tempFile),
 		)
 
-		var previousHeadingDepth int
+		var previousLineIndentationDepth int
 		for {
 			_line, _, err := editor.ReadLine()
 
@@ -112,28 +112,54 @@ func main() {
 			// Convert todo sections to markdown Headings.
 			var isHeading bool
 			Line.content, isHeading = strings.CutSuffix(Line.content, ":")
-			if isHeading {
-				// Headings should have no indentation.
-				Line.content = strings.TrimLeft(Line.content, string(Line.indentationRune))
-				Line.content = strings.Repeat("#", Line.indentationDepth+1) + " " + Line.content
-				// Store the heading depth, for processing the content under it.
-				previousHeadingDepth = Line.indentationDepth
+			// Start by trimming spaces, to remove indentation.
+			Line.content = strings.TrimSpace(Line.content)
+			// Leave code blocks as is.
+			if strings.HasPrefix(Line.content, "```") {
 				writeLine(*editor, Line.content)
 				continue
 			}
+			// Prefix headings with the appropriate number of # characters,
+			// based on the indentation depth.
+			if isHeading {
+				Line.content = strings.Repeat("#", Line.indentationDepth+1) + " " + Line.content
+				writeLine(*editor, Line.content)
+				// Reset previous line depth, since we are in a new heading.
+				// and the next line should not be indented.
+				previousLineIndentationDepth = 0
+				continue
+			}
+			// Non heading lines should be indented based on the previous line.
 			// If the indentation is not the tab character,
 			// consider the indentation to be double the amount runes.
-			if Line.indentationRune != '	' {
-				previousHeadingDepth *= 2
+			if Line.indentationDepth != 0 && Line.indentationRune != 0 && previousLineIndentationDepth != 0 && previousLineIndentationDepth != Line.indentationDepth {
+				if Line.indentationRune != '	' {
+					previousLineIndentationDepth *= 2
+				}
+				Line.content = strings.Repeat(string(Line.indentationRune), previousLineIndentationDepth) + Line.content
 			}
-			for range previousHeadingDepth {
-				Line.content = strings.TrimPrefix(Line.content, string(Line.indentationRune))
-			}
-			// Convert the todo boxes to markdown checkboxes.
-			Line.content = strings.ReplaceAll(Line.content, "☐", "- [ ]")
 
+			// for range previousLineIndentationDepth {
+			// 	Line.content = string(Line.indentationRune) + Line.content
+			// }
+
+			// Convert the todo boxes to markdown checkboxes.
+			Line.content = strings.NewReplacer("✔", "- [x]", "☐", "- [ ]").Replace(Line.content)
+
+			// @TODO: Do I actually need this?
 			if !isHeading && Line.indentationDepth <= 0 && !strings.HasPrefix(Line.content, "-") {
 				Line.content = Line.content + "⚠️"
+			}
+			// At the end of processing the line, store the current indentation depth,
+			// to know if the next line should be indented and how much.
+			fmt.Println("The next line should be indented by the difference of this line and previous line depth")
+			fmt.Println("Should consider if the next line is more or less indented than the previous line")
+			fmt.Println("1.1, 1.2, 1.3, 1.3.1, 1.3.2, 1.3.3, 1.4, 1.5")
+			fmt.Println("In the above sequence, 1.4 should not take 1.3.3 into consideration but 1.3")
+
+			return
+			if previousLineIndentationDepth != Line.indentationDepth {
+				previousLineIndentationDepth = max(Line.indentationDepth-previousLineIndentationDepth, 0)
 			}
 
 			writeLine(*editor, Line.content)
