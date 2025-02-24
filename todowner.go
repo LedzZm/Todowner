42b11683d5d87e9fafd79de749ad59f41b901499
Marios Anagnostopoulos <mariosranag@gmail.com>
2025-02-24 23:42:06 +0200

    Fix indentation issues, presets unknown role lines as list items and adds a warning for those.

--- todowner.go
+++ todowner.go
@@ -11,6 +11,8 @@ import (
 	"unicode/utf8"
 )
 
+var lineReplacements = []string{"✔", "- [x]", "☐", "- [ ]"}
+
 type Line struct {
 	content          string
 	indentationDepth int
@@ -78,7 +80,6 @@ func main() {
 		// Create the full nested filepath inside the backup folder.
 		os.MkdirAll(backupDir+filepath.Dir(filePath), 0770)
 		// Copy the sourceFile contents to the backu file.
-		// backupFile, _ := os.Open(backupDir + filePath)
 		backupFile, _ := os.Create(backupDir + filePath)
 		io.Copy(backupFile, sourceFile)
 		backupFile.Close()
@@ -96,7 +97,7 @@ func main() {
 			bufio.NewWriter(tempFile),
 		)
 
-		var previousLineIndentationDepth int
+		var previousHeadingIndentationDepth int
 		for {
 			_line, _, err := editor.ReadLine()
 
@@ -109,6 +110,11 @@ func main() {
 			if Line.content == "" {
 				continue
 			}
+
+			if Line.content == "＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿" {
+				continue
+			}
+
 			// Convert todo sections to markdown Headings.
 			var isHeading bool
 			Line.content, isHeading = strings.CutSuffix(Line.content, ":")
@@ -124,43 +130,35 @@ func main() {
 			if isHeading {
 				Line.content = strings.Repeat("#", Line.indentationDepth+1) + " " + Line.content
 				writeLine(*editor, Line.content)
-				// Reset previous line depth, since we are in a new heading.
-				// and the next line should not be indented.
-				previousLineIndentationDepth = 0
+				// At the end of processing the line, store the current indentation depth,
+				// to know if the next line should be indented and how much.
+				previousHeadingIndentationDepth = Line.indentationDepth
 				continue
 			}
-			// Non heading lines should be indented based on the previous line.
-			// If the indentation is not the tab character,
-			// consider the indentation to be double the amount runes.
-			if Line.indentationDepth != 0 && Line.indentationRune != 0 && previousLineIndentationDepth != 0 && previousLineIndentationDepth != Line.indentationDepth {
-				if Line.indentationRune != '	' {
-					previousLineIndentationDepth *= 2
-				}
-				Line.content = strings.Repeat(string(Line.indentationRune), previousLineIndentationDepth) + Line.content
+			// Non-heading lines should have their nesting levels reduced
+			// by the number of levels lost by the containing heading.
+			// The old format was based on the premise that:
+			// 1. Headings end with ':'
+			// 2. Content starts one indentation level after the heading.
+			// In markdown this is not needed, so we also remove one more indentation level.
+			// in non-heading lines.
+			newLineIndentation := max(Line.indentationDepth-(previousHeadingIndentationDepth+1), 0)
+			// Non tab character spaces, should add twice the depth to the indentation.
+			if Line.indentationRune != '	' {
+				newLineIndentation *= 2
 			}
-
-			// for range previousLineIndentationDepth {
-			// 	Line.content = string(Line.indentationRune) + Line.content
-			// }
 
 			// Convert the todo boxes to markdown checkboxes.
-			Line.content = strings.NewReplacer("✔", "- [x]", "☐", "- [ ]").Replace(Line.content)
+			Line.content = strings.NewReplacer(lineReplacements...).Replace(Line.content)
 
-			// @TODO: Do I actually need this?
-			if !isHeading && Line.indentationDepth <= 0 && !strings.HasPrefix(Line.content, "-") {
-				Line.content = Line.content + "⚠️"
+			// Add a warning to lines that are not headings and do not start with a dash.
+			// This will pre-emptively mark them as list items, but also notify
+			// the user that they might need to be reviewed.
+			if !isHeading && !strings.HasPrefix(Line.content, "-") {
+				Line.content = "- " + Line.content + "⚠️"
 			}
-			// At the end of processing the line, store the current indentation depth,
-			// to know if the next line should be indented and how much.
-			fmt.Println("The next line should be indented by the difference of this line and previous line depth")
-			fmt.Println("Should consider if the next line is more or less indented than the previous line")
-			fmt.Println("1.1, 1.2, 1.3, 1.3.1, 1.3.2, 1.3.3, 1.4, 1.5")
-			fmt.Println("In the above sequence, 1.4 should not take 1.3.3 into consideration but 1.3")
 
-			return
-			if previousLineIndentationDepth != Line.indentationDepth {
-				previousLineIndentationDepth = max(Line.indentationDepth-previousLineIndentationDepth, 0)
-			}
+			Line.content = strings.Repeat(string(Line.indentationRune), newLineIndentation) + Line.content
 
 			writeLine(*editor, Line.content)
 		}
